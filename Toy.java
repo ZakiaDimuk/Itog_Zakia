6d28ba20cdb22a852c016373e6f7cf7698fac183
ZakiaDimuk <dimukasheva97@mail.ru>
2023-09-21 22:26:38 +0500

    KontrRabJava

---
+++ Toy.java
@@ -0,0 +1,43 @@
+
+public class Toy {
+    private int id;
+    private String name;
+    private int quantity;
+    private double weight;
+
+    /**
+     * @param id
+     * @param name
+     * @param quantity
+     * @param weight
+     */
+    public Toy(int id, String name, int quantity, double weight) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.weight = weight;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getWeight() {
+        return weight;
+    } 
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
